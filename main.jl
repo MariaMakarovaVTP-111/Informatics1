20e3c1d7d36d63f80a8ddcb2125f4312bc3f65e3
Maria Makarova <79919958+MariaMakarovaVTP-111@users.noreply.github.com>
2021-06-23 21:30:02 +0000

    Была написана программа 3-1(Вариант 7) на языке Julia

--- main.jl
+++ main.jl
@@ -1,19 +1,50 @@
-println("Task 1-2, option 7");
+function read_input(message)
+    if (message != nothing)
+        println(message);
+    end;
+    result = readline();
+    return result;
+end;
 
-println("Enter the width of the triangle ");
-width = readline();
+function can_calculate(x, epsilon)::Bool
+    return (1 - x) > epsilon;
+end
 
-println("Enter the length of the triangle  ");
-length = readline();
+function f(x)
+    return (sqrt(1 - x)) - cos(sqrt(1 - x)) ;
+end
 
-width = tryparse(Float64, width);
-length = tryparse(Float64, length);
+println("Задача 3-1. Вариант 7");
 
-P = (width * 2) + (length * 2);
-S = width * length;
-d = sqrt((width ^ 2) + (length ^ 2));
+try
+    local left_bound  = parse(Float64, read_input("Введите левую границу расчета"));
+    local right_bound = parse(Float64, read_input("Введите правую границу расчета"));
+    local step = parse(Float64, read_input("Введите шаг расчета"));
+    
 
-
-println("P = $P");
-println("S = $S");
-println("d = $d");
+    if step <= floatmin() # меньше или равен 0
+        throw("Шаг меньше или равен 0. Ошибка!");
+    end;
+    local x = left_bound;
+    println("|   x     |      y              |");
+    println("|____  ___|_____________________|");
+    while x <= right_bound
+        if can_calculate(x, floatmin())
+            y = f(x);
+            println("| $(round(x, digits = 2))    | $(round(y, digits = 6))|");
+        else
+            println("| $(round(x, digits = 2))    |нет решения              |");
+        end;
+        x = x + step;
+    end;
+catch e
+    println("-----------------------------------------------------------");
+    println("В процессе исполения программы произошла нештатная ситуация");
+    if isa(e, ArgumentError)
+        println(" $(e.msg)")
+    else
+        println(" Неизвестная ошибка")
+        rethrow(e)
+    end;
+    println("-----------------------------------------------------------");
+end
