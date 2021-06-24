a8d1cb59ac9cda52a26298db63010a4a1ef079c6
Maria Makarova <79919958+MariaMakarovaVTP-111@users.noreply.github.com>
2021-06-24 07:58:09 +0000

    Выполнено задание 1-1 на языке Julia

--- main.jl
+++ main.jl
@@ -1,21 +1,8 @@
-println("Task 1-2");
-
-println("Введите значение первой стороны:  ");
-storona1 = readline();
-
-println("Введите значение второй стороны:  ");
-storona2 = readline();
-
-println("Введите значение угла:  ");
-gamma = readline();
-
-storona1 = tryparse(Float64, storona1);
-storona2 = tryparse(Float64, storona2);
-gamma = tryparse(Float64, gamma);
-
-storona3 = sqrt((storona1^2) + (storona2^2)) - (((storona1 * storona2) * 2 * cos(gamma)));
-s = (0.5 * (storona1 * storona2)) * sind(gamma);
-r = (storona1 * storona2 * storona3) / (4 * s);
-println("storona3 = $storona3");
-println("s = $s");
-println("r = $r");
+#run(`clean`);
+x = 0.5;
+y = 0.05;
+z = 0.7;
+a = ((x^2) * ((x+1)) / (y - sin(x + z) * sin(x + z)));
+b = sqrt((x + y) / z) + cos((x + z)^2) * cos((x + y)^2);
+println("x = ", x, "  y = ", y, "  z = ", z);
+println("a = ", a, "  b = ", b);
