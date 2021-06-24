312d72d5c132dde6315784419c2164c8a419983b
Maria Makarova <79919958+MariaMakarovaVTP-111@users.noreply.github.com>
2021-06-24 08:10:33 +0000

    Выполнена задача 1-2 на языке Julia

--- main.jl
+++ main.jl
@@ -1,8 +1,19 @@
-#run(`clean`);
-x = 0.5;
-y = 0.05;
-z = 0.7;
-a = ((x^2) * ((x+1)) / (y - sin(x + z) * sin(x + z)));
-b = sqrt((x + y) / z) + cos((x + z)^2) * cos((x + y)^2);
-println("x = ", x, "  y = ", y, "  z = ", z);
-println("a = ", a, "  b = ", b);
+println("Введите значение первой стороны:  ");
+side1 = readline();
+
+println("Введите значение второй стороны:  ");
+side2 = readline();
+
+println("Введите значение угла:  ");
+corner = readline();
+
+side1 = tryparse(Float64, side1);
+side2 = tryparse(Float64, side2);
+corner = tryparse(Float64, corner);
+
+side3 = sqrt((side1^2) + (side2^2)) - (((side1 * side2) * 2 * cos(corner)));
+s = (0.5 * (side1 * side2)) * sind(corner);
+r = (side1 * side2 * side3) / (4 * s);
+println("side3 = $side3");
+println("s = $s");
+println("r = $r");
